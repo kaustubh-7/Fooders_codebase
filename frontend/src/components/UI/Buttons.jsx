d28485d6dd30239321da91557a78479fb8699825
Kaustubh Bajpai <82926440+kaustubh-7@users.noreply.github.com>
2025-03-04 00:53:50 +0530

    Update Buttons.jsx

--- frontend/src/components/UI/Buttons.jsx
+++ frontend/src/components/UI/Buttons.jsx
@@ -9,7 +9,15 @@ export default function Button({children, textOnly, className, ...props }){
     {...props}
     whileHover={{ scale: 1.1, rotate: 10 }}
     whileTap={{ scale: 0.9 }}
-    transition={{ type: 'spring', stiffness: 300 }}>
+    transition={{ type: 'spring', stiffness: 300 }}
+     style={{
+        padding: '10px 20px',
+        backgroundColor: '#007bff',
+        color: 'white',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+      }}>           
         {children}
     </motion.button>
 }
