f1adfbd130a9acd37ae4040fa04795df5bd71672
Kaustubh Bajpai <82926440+kaustubh-7@users.noreply.github.com>
2025-03-04 00:49:43 +0530

    Update Buttons.jsx
    improving add to cart button

--- frontend/src/components/UI/Buttons.jsx
+++ frontend/src/components/UI/Buttons.jsx
@@ -1,8 +1,15 @@
+import { motion } from 'framer-motion';
+
 export default function Button({children, textOnly, className, ...props }){
     let cssClass = textOnly ? 'text-button' : 'button';
     cssClass += " " +  className;
 
-    return <button className={cssClass} {...props}>
+    return <motion.button 
+    className={cssClass} 
+    {...props}
+    whileHover={{ scale: 1.1, rotate: 10 }}
+    whileTap={{ scale: 0.9 }}
+    transition={{ type: 'spring', stiffness: 300 }}>
         {children}
-    </button>
+    </motion.button>
 }
