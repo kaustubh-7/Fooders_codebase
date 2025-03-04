2f1bb0a624b3cd2857d696d40e5747c8bb552eaa
Kaustubh Bajpai <82926440+kaustubh-7@users.noreply.github.com>
2025-03-04 11:53:33 +0530

    Update Buttons.jsx

--- frontend/src/components/UI/Buttons.jsx
+++ frontend/src/components/UI/Buttons.jsx
@@ -1,14 +1,16 @@
 import { motion } from 'framer-motion';
 
-export default function Button({children, textOnly, className, ...props }){
+export default function Button({children, textOnly, className, isCart, ...props }){
     let cssClass = textOnly ? 'text-button' : 'button';
     cssClass += " " +  className;
+
+    const effect = isCart ? { color: 'yellow' } : { rotate: 3, backgroundColor: 'yellow' };
 
     return <motion.button 
     className={cssClass} 
     {...props}
-    whileHover={{ scale: 1.08, rotate: 3, backgroundColor: 'yellow', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',  }}
-    whileTap={{ scale: 0.999, backgroundColor: '#ffc404' }}
+    whileHover={{ scale: 1.08,...effect , boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',  }}
+    whileTap={{ scale: 0.8, backgroundColor: '#ffc404' }}
     transition={{ type: 'spring', stiffness: 200 }}
     >
         {children}
