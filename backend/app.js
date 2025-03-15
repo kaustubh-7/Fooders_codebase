495fb16084167aed20ea98dcc7921ca836d65826
Kaustubh Bajpai <82926440+kaustubh-7@users.noreply.github.com>
2025-03-15 14:11:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://fooders-codebase.onrender.com");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
